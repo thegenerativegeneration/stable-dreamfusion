c006822a933caf3a653f9c788747771f5dcc3852
TGG <phhaslbauer@yahoo.de>
2023-05-29 20:00:11 +0200

    update dl script

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -3,3 +3,5 @@
 set -e
 
 wget https://huggingface.co/cvlab/zero123-weights/resolve/main/105000.ckpt -O pretrained/zero123/105000.ckpt
+wget https://huggingface.co/thegenerativegeneration/omnidata/resolve/main/omnidata_dpt_depth_v2.ckpt -O pretrained/omnidata/omnidata_dpt_depth_v2.ckpt
+wget https://huggingface.co/thegenerativegeneration/omnidata/resolve/main/omnidata_dpt_normal_v2.ckpt -O pretrained/omnidata/omnidata_dpt_normal_v2.ckpt
