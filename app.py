596983e81af52bf1f2afd29dfa828b5f4e1ad3ab
TGG <phhaslbauer@yahoo.de>
2023-05-29 18:21:43 +0200

    add dockerfile for models

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+import glob
+import os
 import sys
+import time
 
+import numpy as np
 import torch
-import argparse
+import subprocess
 
 from nerf.provider import NeRFDataset
-from nerf.utils import *
+from nerf.utils import Trainer, seed_everything
 
 import gradio as gr
 import gc
 from params import parser
 from main import prepare
+
+# todo: add mesh export and download
 
 opt = parser.parse_args()
 
@@ -42,22 +48,37 @@ print(f'[INFO] everything loaded!')
 
 trainer = None
 model = None
-
 opt.test = True
 
-# define UI
+guidance_combinations = [
+    'SD',
+    'IF',
+    'zero123',
+    'SD,clip',
+]
 
+guidance_combinations_labels = [
+    'Text-to-3D (Stable-Diffusion)',
+    'Text-to-3D (DeepFloyd IF)',
+    'Image-to-3D (zero123)',
+    'Text+Image-to-3D (Stable-Diffusion + CLIP)'
+]
+
+# define UI
 with gr.Blocks(css=".gradio-container {max-width: 512px; margin: auto;}") as demo:
     # title
     gr.Markdown('[Stable-DreamFusion](https://github.com/ashawkey/stable-dreamfusion) Text-to-3D Example')
 
     # inputs
     prompt = gr.Textbox(label="Prompt", max_lines=1, value="a DSLR photo of a koi fish")
+    input_image = gr.Image(label="Input Image", visible=False, type="filepath")
+
     iters = gr.Slider(label="Iters", minimum=1000, maximum=20000, value=5000, step=100)
     seed = gr.Slider(label="Seed", minimum=0, maximum=2147483647, step=1, randomize=True)
     # allow multiple selections for guidance
-    guidance_type = gr.CheckboxGroup(label="Guidance", choices=["SD", "IF", "zero123", "clip"],
-                                      default=["SD"])
+    guidance_type = gr.Dropdown(choices=guidance_combinations_labels, label="Modality",  type="index", default=0)
+    workspace = gr.Textbox(label="Workspace (output folder)", max_lines=1, value="workspace")
+
     button = gr.Button('Generate')
 
     # outputs
@@ -67,7 +88,7 @@ with gr.Blocks(css=".gradio-container {max-width: 512px; margin: auto;}") as dem
 
 
     # gradio main func
-    def submit(text, iters, seed, guidance_type):
+    def submit(text, input_image, iters, seed, guidance_type, workspace):
 
         global trainer, model, opt
 
@@ -80,19 +101,43 @@ with gr.Blocks(css=".gradio-container {max-width: 512px; margin: auto;}") as dem
             torch.cuda.empty_cache()
             print('[INFO] clean up!')
 
+
         # prepare
-        # seed
         opt.seed = seed
         opt.text = text
         opt.iters = iters
-        opt.guidance = guidance_type
+        opt.guidance = guidance_combinations[guidance_type].split(',')
+        opt.workspace = workspace
+
+        if 'zero123' in opt.guidance:
+            opt.text = None
+        elif 'clip' not in opt.guidance:
+            opt.image = None
+
+        # preprocess image
+        if input_image is not None:
+            process = subprocess.Popen(
+                ["python3", "preprocess_image.py", input_image]
+            )
+            process.wait()
+
+            if process.returncode != 0:
+                raise Exception("preprocess_image.py failed")
+
+            input_image = input_image.replace('.png', '_rgba.png')
+
+
+
+        opt.image = input_image
+
+        print("image", input_image)
         if 'IF' in opt.guidance:
             opt.IF = True
         seed_everything(seed)
 
         model, _, opt = prepare(opt)
 
-        guidance = nn.ModuleDict()
+        guidance = torch.nn.ModuleDict()
 
         if 'SD' in opt.guidance:
             from guidance.sd_utils import StableDiffusion
@@ -111,13 +156,28 @@ with gr.Blocks(css=".gradio-container {max-width: 512px; margin: auto;}") as dem
             guidance['clip'] = CLIP(device)
 
         # simply reload everything...
-        optimizer = lambda model: torch.optim.Adam(model.get_params(opt.lr), betas=(0.9, 0.99), eps=1e-15)
-        scheduler = lambda optimizer: optim.lr_scheduler.LambdaLR(optimizer,
-                                                                  lambda iter: 0.1 ** min(iter / opt.iters, 1))
+        #optimizer = lambda model: torch.optim.Adam(model.get_params(opt.lr), betas=(0.9, 0.99), eps=1e-15)
+        #scheduler = lambda optimizer: optim.lr_scheduler.LambdaLR(optimizer,
+        #                                                          lambda iter: 0.1 ** min(iter / opt.iters, 1))
+
+        if opt.optim == 'adan':
+            from optimizer import Adan
+            # Adan usually requires a larger LR
+            optimizer = lambda model: Adan(model.get_params(5 * opt.lr), eps=1e-8, weight_decay=2e-5, max_grad_norm=5.0, foreach=False)
+        else: # adam
+            optimizer = lambda model: torch.optim.Adam(model.get_params(opt.lr), betas=(0.9, 0.99), eps=1e-15)
+
+        if opt.backbone == 'vanilla':
+            scheduler = lambda optimizer: torch.optim.lr_scheduler.LambdaLR(optimizer, lambda iter: 0.1 ** min(iter / opt.iters, 1))
+        else:
+            scheduler = lambda optimizer: torch.optim.lr_scheduler.LambdaLR(optimizer, lambda iter: 1) # fixed
+            # scheduler = lambda optimizer: optim.lr_scheduler.LambdaLR(optimizer, lambda iter: 0.1 ** min(iter / opt.iters, 1))
 
         trainer = Trainer(' '.join(sys.argv), 'df', opt, model, guidance, device=device, workspace=opt.workspace, optimizer=optimizer,
                           ema_decay=0.95, fp16=opt.fp16, lr_scheduler=scheduler, use_checkpoint=opt.ckpt,
                           scheduler_update_every_step=True)
+
+        trainer.default_view_data = train_loader._data.get_default_view_data()
 
         # train (every ep only contain 8 steps, so we can get some vis every ~10s)
         STEPS = 8
@@ -167,7 +227,7 @@ with gr.Blocks(css=".gradio-container {max-width: 512px; margin: auto;}") as dem
         trainer.test(test_loader)
 
         results = glob.glob(os.path.join(opt.workspace, 'results', '*rgb*.mp4'))
-        assert results is not None, "cannot retrieve results!"
+        assert results, "cannot retrieve results!"
         results.sort(key=lambda x: os.path.getmtime(x))  # sort by mtime
 
         end_t = time.time()
@@ -181,11 +241,36 @@ with gr.Blocks(css=".gradio-container {max-width: 512px; margin: auto;}") as dem
 
     button.click(
         submit,
-        [prompt, iters, seed, guidance_type],
+        [prompt, input_image, iters, seed, guidance_type, workspace],
         [image, video, logs]
+    )
+
+    def on_guidance_type_change(guidance_index):
+
+        guidance_type = guidance_combinations[guidance_index]
+        if 'zero123' in guidance_type:
+            return {
+                input_image: gr.update(visible=True),
+                prompt: gr.update(visible=False)
+            }
+        elif 'SD,clip' in guidance_type:
+            return {
+                input_image: gr.update(visible=True),
+                prompt: gr.update(visible=True)
+            }
+        else:
+            return {
+                input_image: gr.update(visible=False),
+                prompt: gr.update(visible=True)
+            }
+
+    guidance_type.change(
+        on_guidance_type_change,
+        [guidance_type],
+        [input_image, prompt]
     )
 
 # concurrency_count: only allow ONE running progress, else GPU will OOM.
 demo.queue(concurrency_count=1)
 
-demo.launch()
+demo.launch(server_name="0.0.0.0")
